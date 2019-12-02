c6a4030c7233c7fc1467708398118ea588e16c08
leoa <leoa@theodo.fr>
2019-12-02 14:57:10 +0100

    Add addons

--- frontend/src/components/Button/Button.stories.js
+++ frontend/src/components/Button/Button.stories.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withKnobs, boolean, text } from '@storybook/addon-knobs';
+import { withA11y } from '@storybook/addon-a11y';
+import { withConsole } from '@storybook/addon-console';
 import Button from './Button';
 
 storiesOf('Button', module)
-  .add('Standard', () => <Button onClick={() => console.log('Hello !')}>Hello Button</Button>)
-  .add('Disabled', () => (
-    <Button disabled onClick={() => console.log('Hello !')}>
-      Hello Button
+  .addDecorator(withKnobs)
+  .addDecorator(withA11y)
+  .addDecorator((storyFn, context) => withConsole()(storyFn)(context))
+  .add('Standard', () => (
+    <Button disabled={boolean('Disabled', false)} onClick={() => console.log('Hello !')}>
+      {text('Button label', 'Awesome button')}
     </Button>
   ));
